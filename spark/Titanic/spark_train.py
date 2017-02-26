495322da4e69ab740d93aac3c2db64d2e38d6940
browha <hafbrown@gmail.com>
2017-02-26 19:29:07 +0000

    spark_train.py for Titanic
    Some code I wrote about a year ago, which demonstrates the Titanic example from Kaggle using Spark and DecisionTrees. I have not tried to do any feature engineering, etc, over the dataset in any significant way, the purpose of this is just to show how to use Spark to do a classification problem.

---
+++ spark/Titanic/spark_train.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from pyspark import SparkContext
+from pyspark.sql import SQLContext
+from pyspark.mllib.regression import LabeledPoint
+import array
+
+sc=SparkContext("local","titanic_test")
+sqlContext = SQLContext(sc)
+df=pd.read_csv('Titanic_train.csv')
+
+df['Sex']=df['Sex'].replace('female',1)
+df['Sex']=df['Sex'].replace('male',0)
+df['Age']=df['Age'].replace('NaN',-1)
+
+traindf=pd.DataFrame(df,columns=['Survived','Pclass','Age','Sex','Fare'])
+
+sdf=sqlContext.createDataFrame(traindf)
+
+
+import pyspark.mllib.classification as sparkclass
+temp = sdf.map(lambda x:LabeledPoint(x[0],[x[1:]]))
+
+
+
+#lrm = sparkclass.SVMWithSGD.train(temp,iterations=10)
+#lrm=sparkclass.LogisticRegressionWithSGD.train(temp,iterations=10)
+
+from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
+
+lrm=DecisionTree.trainClassifier(temp,numClasses=2, categoricalFeaturesInfo={}, impurity='gini', maxDepth=5, maxBins=32)
+
+df=pd.read_csv('Titanic_test.csv')
+
+df['Sex']=df['Sex'].replace('female',1)
+df['Sex']=df['Sex'].replace('male',0)
+df['Age']=df['Age'].replace('NaN',-1)
+
+testdf=pd.DataFrame(df,columns=['Pclass','Age','Sex','Fare'])
+
+testsdf=sqlContext.createDataFrame(testdf)
+
+testtemp = testsdf.map(lambda x:x[0:])
+
+test_predictions=lrm.predict(testtemp).collect()
+
+
+
+print(test_predictions)
