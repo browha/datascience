f2ba8da88b62fc1f0cb6c21e2254af92935e4d4c
browha <hafbrown@gmail.com>
2017-02-28 19:16:03 +0000

    Update spark_train.py

--- spark/Titanic/spark_train.py
+++ spark/Titanic/spark_train.py
@@ -15,18 +15,11 @@ df['Age']=df['Age'].replace('NaN',-1)
 traindf=pd.DataFrame(df,columns=['Survived','Pclass','Age','Sex','Fare'])
 
 sdf=sqlContext.createDataFrame(traindf)
-
-
 import pyspark.mllib.classification as sparkclass
 temp = sdf.map(lambda x:LabeledPoint(x[0],[x[1:]]))
-
-
-
-#lrm = sparkclass.SVMWithSGD.train(temp,iterations=10)
-#lrm=sparkclass.LogisticRegressionWithSGD.train(temp,iterations=10)
+#Spark uses LabeledPoints. That is to say, for a particular set of features (x[1:]), we have a classification x[0]
 
 from pyspark.mllib.tree import DecisionTree, DecisionTreeModel
-
 lrm=DecisionTree.trainClassifier(temp,numClasses=2, categoricalFeaturesInfo={}, impurity='gini', maxDepth=5, maxBins=32)
 
 df=pd.read_csv('Titanic_test.csv')
@@ -36,13 +29,7 @@ df['Sex']=df['Sex'].replace('male',0)
 df['Age']=df['Age'].replace('NaN',-1)
 
 testdf=pd.DataFrame(df,columns=['Pclass','Age','Sex','Fare'])
-
 testsdf=sqlContext.createDataFrame(testdf)
-
 testtemp = testsdf.map(lambda x:x[0:])
-
 test_predictions=lrm.predict(testtemp).collect()
-
-
-
 print(test_predictions)
